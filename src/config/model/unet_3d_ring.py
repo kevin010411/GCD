be25c826f2470d0149e25aaa59b695bf6649ebe5
annaljs <86973211+annaljs@users.noreply.github.com>
2026-09-01 11:59:32 +0800

    新增一個實驗的config

---
+++ src/config/model/unet_3d_ring.py
@@ -0,0 +1,11 @@
+_base_ = ["../base.py"]
+
+model = dict(
+    type="UNet",
+    act="RELU",
+    norm="BATCH",
+    out_channels=4,
+)  # 模型
+# ckpt = "checkpoint/3d_unet_2025.pth"  # 權重檔
+ckpt = "checkpoint/60_20_20_fold3/unet3d_ring_cue.pth"  # 權重檔
+default_layer = "decoder 1"  # 預設 CAM 層
